e299006b105f1713769ff5e56e88c7f3bebf6309
anhyeuemm <busganojohnandrei@gmail.com>
2025-05-30 13:23:12 +0800

    Exception handling

--- lib/widgets/picker_body.dart
+++ lib/widgets/picker_body.dart
@@ -18,7 +18,7 @@ class NetworkImagePickerBody extends StatelessWidget {
         future: _imageRepo.getNetworkImages(),
         builder: (BuildContext context,
             AsyncSnapshot<List<PixelfordImage>> snapshot) {
-          if (snapshot.hasData)
+          if (snapshot.hasData) {
             return GridView.builder(
               itemCount: snapshot.data!.length,
               itemBuilder: (context, index) {
@@ -34,6 +34,12 @@ class NetworkImagePickerBody extends StatelessWidget {
                 maxCrossAxisExtent: MediaQuery.of(context).size.width * 0.5,
               ),
             );
+          } else if (snapshot.hasError) {
+            return Padding(
+              padding: const EdgeInsets.all(24.0),
+              child: Text('This is the error: ${snapshot.error}'),
+            );
+          }
           //return Image.network(snapshot.data![0].urlFullSize);
           return Padding(
             padding: const EdgeInsets.all(8.0),
