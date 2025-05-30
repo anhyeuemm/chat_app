e394bbc126f13e75cbd7a330d683c9fe857e933c
anhyeuemm <busganojohnandrei@gmail.com>
2025-05-30 09:37:21 +0800

    Adding plugins to your app from pub dev

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:chat_app_eve/widgets/login_text_field.dart';
 //import 'package:chat_app_eve/chat_page.dart';
 import 'package:chat_app_eve/utils/spaces.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class LoginPage extends StatelessWidget {
   LoginPage({Key? key}) : super(key: key);
@@ -27,6 +28,7 @@ class LoginPage extends StatelessWidget {
 
   final userNameController = TextEditingController();
   final passwordController = TextEditingController();
+  final _mainUrl = 'https://poojabhaumik.com';
 
   @override
   Widget build(BuildContext context) {
@@ -100,14 +102,17 @@ class LoginPage extends StatelessWidget {
                           TextStyle(fontSize: 24, fontWeight: FontWeight.w300),
                     )),
                 GestureDetector(
-                  onTap: () {
+                  onTap: () async {
                     //todo: Navigate to browser
                     print('Link clicked!');
+                    if (!await launch(_mainUrl)) {
+                      throw 'Could not launch this';
+                    }
                   },
                   child: Column(
                     children: [
                       Text('Find us on'),
-                      Text('https://poojabhaumik.com'),
+                      Text(_mainUrl),
                     ],
                   ),
                 )
